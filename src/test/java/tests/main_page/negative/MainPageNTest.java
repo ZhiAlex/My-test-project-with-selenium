890b49228aa541a4ab474652d12df1c8f7913cf6
Alexander Zhibrailov <zhialex.ru@gmail.com>
2021-09-08 13:33:33 +0300

    added a class for negative testing of the main page

---
+++ src/test/java/tests/main_page/negative/MainPageNTest.java
@@ -0,0 +1,28 @@
+package tests.main_page.negative;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import tests.base.BaseTest;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static constants.Constant.TimeoutVariables.IMPLICIT_WAIT;
+import static constants.Constant.Urls.BASE_URL;
+
+public class MainPageNTest extends BaseTest {
+
+    @Test()
+    public void SendEmptySearchInput1(){
+        basePage.goToUrl(BASE_URL);
+        mainPage.clickSearchButton();
+        basePage.waitElementIsVisible(driver.findElement(By.xpath("//h1[text()='Поиск аниме по названию']")));
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        List<WebElement> items =  driver.findElements(By.xpath("//div[@class='items']"));
+        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+        Assertions.assertTrue(items.isEmpty());
+    }
+
+}
