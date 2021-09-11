61842fc1f3723cdceffd1df004cb3494620787be
Alexander Zhibrailov <zhialex.ru@gmail.com>
2021-09-11 20:51:18 +0300

    Add new elements and methods to them

--- src/main/java/pages/manga_page/MangaPage.java
+++ src/main/java/pages/manga_page/MangaPage.java
@@ -2,7 +2,10 @@ package pages.manga_page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pages.base.BasePage;
+
+import java.util.List;
 
 public class MangaPage extends BasePage {
 
@@ -12,6 +15,10 @@ public class MangaPage extends BasePage {
 
     private final By ruAnimeName = By.xpath("//h2[@class='line-1']");
     private final By enAnimeName = By.xpath("//h2[@class='line-2']");
+    private final By tags = By.xpath("//div[@class='m-genres-list']/child::a");
+    private final By startWatchingButton = By.xpath("//div[@class='col s12 m8 l9 col-even']/a");
+    private final By viewOrderBlock = By.xpath("//div[@class='card m-connections-list']");
+    private final By allReleasedEpisodesButtons = By.xpath("//div[@class='card-content m-episode-list']/div/div/a");
 
     public String getRuAnimeName(){
         return driver
@@ -23,6 +30,22 @@ public class MangaPage extends BasePage {
         return driver
                 .findElement(enAnimeName)
                 .getText();
+    }
+
+    public List<WebElement> getAnimeTags(){
+        return driver.findElements(tags);
+    }
+
+    public void clickStartWatchingButton(){
+        driver.findElement(startWatchingButton).click();
+    }
+
+    public WebElement getViewOrderBlock(){
+        return driver.findElement(viewOrderBlock);
+    }
+
+    public List<WebElement> getAllReleasedEpisodes(){
+        return driver.findElements(allReleasedEpisodesButtons);
     }
 
 }
