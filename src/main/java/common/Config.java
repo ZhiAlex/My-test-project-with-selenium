921fc54cd92ec799078dcd2acf8ee164b5f3bf5e
Alexander Zhibrailov <zhialex.ru@gmail.com>
2021-09-12 18:11:33 +0300

    add screenshot creation

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -6,5 +6,9 @@ public class Config {
     public static final Boolean CLEAR_COOKIES = true;
     //To keep the browser open after all scenario/tests
     public static final Boolean HOLD_BROWSER_OPEN = false;
+    //Clear the directory with screenshots before starting the build
+    public static final Boolean CLEAR_SCREENSHOT_DIR = true;
+    //Clear the directory with screenshots before starting the build
+    public static final Boolean CLEAR_ALLURE_RESULTS_DIR = true;
 
 }
